4dcaa5d4c32bbefe5d1341f0067e567d46f16551
CKHuang-public <chihkaihuang1@gmail.com>
2025-05-04 21:50:08 +0200

    ww

--- cluster-test/delcluster/delmanagement.py
+++ cluster-test/delcluster/delmanagement.py
@@ -1,11 +1,13 @@
 from enoslib.api import generate_inventory,run_ansible
 from enoslib.infra.enos_vmong5k.provider import VMonG5k
 from enoslib.infra.enos_vmong5k.configuration import Configuration
+import time
 import enoslib as en
+from datetime import datetime
 
 en.set_config(ansible_forks=100)
 
-name = "s1-management-1"
+name = "devs1-management-1"
 
 clusters = "ecotype"
 
@@ -16,6 +18,10 @@ master_nodes = []
 duration = "12:00:00"
 
 prod_network = en.G5kNetworkConf(type="prod", roles=["my_network"], site=site)
+
+today = datetime.now().strftime("%Y-%m-%d")
+
+reservation_time = today + " 17:01:00"
 
 name_job = name + clusters
 
@@ -28,7 +34,7 @@ conf = (
         id="not_linked_to_any_machine", type="slash_22", roles=["my_subnet"], site=site
     )
     .add_machine(
-    roles=["role0"], cluster=clusters, nodes=1, primary_network=prod_network,servers=[f"ecotype-{i}.nantes.grid5000.fr" for i in range(2, 47)]
+    roles=["role0"], cluster=clusters, nodes=1, primary_network=prod_network
     )
     .finalize()
 )
