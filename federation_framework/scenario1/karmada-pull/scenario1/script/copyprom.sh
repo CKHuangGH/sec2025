c37ef2bec739e5c9870ce9765b8a1da9be290d32
CKHuang-public <chihkaihuang1@gmail.com>
2025-05-27 00:04:47 +0200

    Update copyprom.sh

--- federation_framework/scenario1/karmada-pull/scenario1/script/copyprom.sh
+++ federation_framework/scenario1/karmada-pull/scenario1/script/copyprom.sh
@@ -10,5 +10,5 @@ do
 	j=$((j+1))	
 done
 
-scp -o StrictHostKeyChecking=no -r /root/prom-$number-member/ chuang@172.16.207.100:/home/chuang/prombackup-$number-$time-karmada-pull-member
-scp -o StrictHostKeyChecking=no -r /root/prom-$number/ chuang@172.16.207.100:/home/chuang/prombackup-$number-$time-karmada-pull
+scp -o StrictHostKeyChecking=no -r /root/prom-$number-member/ chuang@172.16.79.101:/home/chuang/prombackup-$number-$time-karmada-pull-member
+scp -o StrictHostKeyChecking=no -r /root/prom-$number/ chuang@172.16.79.101:/home/chuang/prombackup-$number-$time-karmada-pull
