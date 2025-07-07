440fee3ce8acf5208a09f21f46049cd2ed50f94e
CKHuangGH <chihkaihuang1@gmail.com>
2025-07-07 15:13:41 +0200

    Update vm-member.py

--- clusters_deployment/now-small/script/vm-member.py
+++ clusters_deployment/now-small/script/vm-member.py
@@ -20,21 +20,19 @@ w=3
 print(list(subnet[0].free_macs)[1:2])
 
 virt_conf = (
-    en.VMonG5kConf.from_settings()
+    en.VMonG5kConf.from_settings(image="/home/chuang/images/large_debian02072025.qcow2")
     .add_machine(
         roles=["cp"],
         number=cp,
         undercloud=roles["role0"],
         flavour_desc={"core": 16, "mem": 32768},
-        image="/home/chuang/images/large_debian02072025.qcow2",
-        macs=list(subnet[0].frㄋee_macs)[1:2],
+        macs=list(subnet[0].free_macs)[1:2],
     )
     .add_machine(
         roles=["member"],
         number=w,
         undercloud=roles["role0"],
         flavour_desc={"core": 2, "mem": 4096},
-        image="/home/chuang/images/debian02072025.qcow2",
         macs=list(subnet[0].free_macs)[2:w+2],
     ).finalize()
 )
