f7cc11e8f0a32a248d42d8db1a14c6953094267c
CKHuangGH <chihkaihuang1@gmail.com>
2025-07-22 11:13:57 +0200

    Update worker_node.sh

--- federation_framework/scenario1/liqo/worker_node.sh
+++ federation_framework/scenario1/liqo/worker_node.sh
@@ -4,6 +4,9 @@ cluster=$1
 mkdir /var/log/ntpsec
 pip3 install kubernetes --break-system-packages
 
+curl --fail -LS "https://github.com/liqotech/liqo/releases/download/v1.0.1/liqoctl-linux-amd64.tar.gz" | tar -xz
+sudo install -o root -g root -m 0755 liqoctl /usr/local/bin/liqoctl
+
 sed -i 's|--listen-metrics-urls=http://127.0.0.1:2381|--listen-metrics-urls=http://0.0.0.0:2381|' "/etc/kubernetes/manifests/etcd.yaml"
 sleep 30
 sed -i 's|--bind-address=127.0.0.1|--bind-address=0.0.0.0|' "/etc/kubernetes/manifests/kube-scheduler.yaml"
